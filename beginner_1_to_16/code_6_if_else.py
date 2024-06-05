164e989e5b4f19e6aac5c5f9d97c54072bba48a1
aimG313 <157086365+aimG313@users.noreply.github.com>
2024-06-06 00:00:16 +0600

    Update code_6_if_else.py

--- beginner_1_to_16/code_6_if_else.py
+++ beginner_1_to_16/code_6_if_else.py
@@ -4,3 +4,18 @@ if num%2 == 0:
   print("even")
 else:
   print("odd")
+
+'''indian = ["samosa","dal","naan"]
+chinese = ["egg","pot","fried rice"
+italian = ["pizza","pasta"]
+
+dish = input("Enter a dish name:")
+
+if dish in indian:
+  print("indian")
+elif dish in chinese:
+  print("chinese")
+elif dish in italian:
+  print("italian")
+else:
+  print("not available")'''
