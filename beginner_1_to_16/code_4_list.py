a166115d46765f5f8452d4630568ab4f9e24d205
aimG313 <157086365+aimG313@users.noreply.github.com>
2024-06-05 23:06:52 +0600

    day-2

---
+++ beginner_1_to_16/code_4_list.py
@@ -0,0 +1,14 @@
+food = ["bread", "pasta", "fruits", "veggies"]
+print(food)
+print(food[1])
+
+food[1]="mango"
+print(food)
+
+food.append("burger")
+print(food)
+
+food.insert(2,"sauce")
+print(len(food))
+
+print("burger" in food)
---
+++ beginner_1_to_16/code_5_debugging.py
@@ -0,0 +1,5 @@
+base = 5
+height = 10
+
+area = 0.5*base*height
+print(area)
